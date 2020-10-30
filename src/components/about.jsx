2f4be008a8610661e276e6869fd9ecaed54cbea2
Pato Miner <patominer@gmail.com>
2020-10-30 15:27:14 +1300

    First Commit

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -13,8 +13,9 @@ export default class About extends Component {
                     <div className="about-desc">
                     <span className="heading-meta">About Us</span>
                     <h2 className="colorlib-heading">Who Am I?</h2>
-                    <p>I am a computer grad student from DAIICT-Gandhinagar with ICT as my major.I love exploring new technologies and often amazed by the progress we as a human species have mad so far in the recent years (apart from the headphone-jack part!!)</p>
-                    <p>I have started reflecting my ideas and thougths through the medium of words recently so spelling and grammer mistaks are very often.You can write me back if you spot any and don't want to live anymore :P </p>
+                    <p>I'm a Senior SDET from Argentina living in New Zealand and loving every aspect of it! After 3 years of working in the local scene, I've decided to put my expertise and knowledge as a service to deliver the best testing solutions and enable a fast paced workflow!</p>
+                    <p>I'm a professional coach and consultant based in the beautiful city of Wellington, where I teach the ways of Automation and Test Engineering, as well as audit and enhance the Automation mahi for my clients! </p>
+                    <p>Monthly, I'm hosting Automation Bootcamps and Workshops both through online webinas and physically across New Zealand. Check below for more details about dates, prices and what you will be learning! </p>
                     </div>
                 </div>
                 </div>
@@ -27,78 +28,80 @@ export default class About extends Component {
             <div className="row">
             <div className="col-md-6 col-md-offset-3 col-md-pull-3 animate-box" data-animate-effect="fadeInLeft">
                 <span className="heading-meta">What I do?</span>
-                <h2 className="colorlib-heading">Here are some of my expertise</h2>
+                <h2 className="colorlib-heading">Here are some of my services</h2>
             </div>
             </div>
             <div className="row row-pt-md">
             <div className="col-md-4 text-center animate-box">
                 <div className="services color-1">
                 <span className="icon">
-                    <i className="icon-bulb" />
+                    <i className="icon-chart-bar" />
                 </span>
                 <div className="desc">
-                    <h3>Web Development </h3>
-                    <p>I have experience building websites and chrome extentions using JavaScript,React,HTML,CSS</p>
+                    <h3>E2E Automation </h3>
+                    <p>More than 10 years developing Test solutions and Automating test cases using JavaScript, C#, Java/Groovy and Python with tools like Selenium and Cucumber, PyTest, Katalon Studio, TestProject, Cypress and TestCafe.</p>
+                    <p ><a className="btn btn-primary btn-learn" href="https://docs.google.com/document/d/15coSa2rnZcdywihM-pWTvoGqE65JAkxQAh6JDBNrF2Q/edit?usp=sharing" target="_blank" rel="noopener noreferrer">View CV<i className="icon-download4" /></a></p>
                 </div>
                 </div>
             </div>
             <div className="col-md-4 text-center animate-box">
                 <div className="services color-3">
                 <span className="icon">
-                    <i className="icon-phone3" />
+                    <i className="icon-code" />
                 </span>
                 <div className="desc">
-                    <h3>Data Structures & Algorithms</h3>
-                    <p>As coming from the CS background, I have good grasp over fundamental concepts of DSA</p>
+                    <h3>Test Engineering</h3>
+                    <p>I can build the whole Automation workflow from scratch and with different tools depending your needs. From frameworks for API Testing, to UI Automation, to the endgame of CI with Jenkins, Docker and dashboard tools for reporting.</p>
+                    <p><a className="btn btn-primary btn-learn" href="https://github.com/thefreerangetester" target="_blank" rel="noopener noreferrer">View Projects <i className="icon-briefcase3" /></a></p>
                 </div>
                 </div>
             </div>
             <div className="col-md-4 text-center animate-box">
                 <div className="services color-5">
                 <span className="icon">
-                    <i className="icon-data" />
+                    <i className="icon-eye" />
                 </span>
                 <div className="desc">
                     <h3>Dev Ops</h3>
-                    <p>I am pursuing my internship with DevOps team at Juniper and working with tools like Jenkins, Docker, K8s</p>
+                    <p>Shifting the test approach left and right to have the right checks at the right time. From monitoring the use of Jenkins with Kibana, to display the user experience in Production with dashboards in Grafana, I can build the right tool for your DevOps testing needs.</p>
+                    <p><a className="btn btn-primary btn-learn" href="https://www.eventbrite.co.nz/e/road-to-test-engineer-automation-and-performance-bootcamp-tickets-126480050235" target="_blank" rel="noopener noreferrer">Next Automation Bootcamp <i className="icon-book" /></a></p>
                 </div>
                 </div>
             </div>
-            {/*
             <div className="col-md-4 text-center animate-box">
                 <div className="services color-2">
                 <span className="icon">
-                    <i className="icon-data" />
+                    <i className="icon-bulb" />
                 </span>
                 <div className="desc">
-                    <h3>Dev Ops</h3>
-                    <p>Jenkins , Kubernetes , Docker </p>
+                    <h3>Teaching and career counsealing</h3>
+                    <p>Helping New Zealand create the pool of engineers to cover the high demand. If you are looking for a new job to be passionate about, there is no better place to look at! </p>
+                    <p><a className="btn btn-primary btn-learn" href="emailto:thefreerangetester@gmail.com" target="_blank" rel="noopener noreferrer">Send an email <i className="icon-briefcase3" /></a></p>
                 </div>
                 </div>
             </div>
             <div className="col-md-4 text-center animate-box">
                 <div className="services color-4">
                 <span className="icon">
-                    <i className="icon-layers2" />
+                    <i className="icon-check" />
                 </span>
                 <div className="desc">
-                    <h3>Graphic Design</h3>
-                    <p>My friend knows .. P</p>
+                    <h3>Automation UI</h3>
+                    <p>From PyTest for Python, to Selenium with Cucumber for Java, passing to Cypress, TestCafe, Katalon and TestProject, I'm constantly creating frameworks on different techstacks.</p>
                 </div>
                 </div>
             </div>
             <div className="col-md-4 text-center animate-box">
                 <div className="services color-6">
                 <span className="icon">
-                    <i className="icon-phone3" />
+                    <i className="icon-data" />
                 </span>
                 <div className="desc">
-                    <h3>Digital Marketing</h3>
-                    <p>I use Instagram eight hours a day :) </p>
+                    <h3>Automation API</h3>
+                    <p>Following the best practices, I create solid frameworks for API Automation with Java and RestAssured, as well as Katalon, Postman, Karate and Request for Python.</p>
                 </div>
                 </div>
             </div>
-            */}
             </div>
         </div>
         </section>
--- src/components/introduction.jsx
+++ src/components/introduction.jsx
@@ -7,45 +7,42 @@ export default class Introduction extends Component {
         <section id="colorlib-hero" className="js-fullheight" data-section="home">
           <div className="flexslider js-fullheight">
             <ul className="slides">
-              <li style={{backgroundImage: 'url(images/img_bg.jpg)'}}>
+              <li style={{backgroundImage: 'url(images/bg_queenstown.jpg)'}}>
                 <div className="overlay" />
                 <div className="container-fluid">
                   <div className="row">
                     <div className="col-md-6 col-md-offset-3 col-md-pull-3 col-sm-12 col-xs-12 js-fullheight slider-text">
                       <div className="slider-text-inner js-fullheight">
                         <div className="desc">
-                          <h1>Hi! <br />I'm Dhruv</h1>
-                          <p><a className="btn btn-primary btn-learn" href="https://docs.google.com/document/d/15coSa2rnZcdywihM-pWTvoGqE65JAkxQAh6JDBNrF2Q/edit?usp=sharing" target="_blank" rel="noopener noreferrer">View CV<i className="icon-download4" /></a></p>
+                          <h1 style={{ color:'black', background:'rgba(237,235,111,0.5)'}}>Kia ora <br/>traveller</h1>
                         </div>
                       </div>
                     </div>
                   </div>
                 </div>
               </li>
-              <li style={{backgroundImage: 'url(images/img_bg.jpg)'}}>
+              <li style={{backgroundImage: 'url(images/devices_bg.jpg)'}}>
                 <div className="overlay" />
                 <div className="container-fluid">
                   <div className="row">
                     <div className="col-md-6 col-md-offset-3 col-md-pull-3 col-sm-12 col-xs-12 js-fullheight slider-text">
                       <div className="slider-text-inner">
                         <div className="desc">
-                          <h1>I love building<br /> THINGS !!</h1>
-                          <p><a className="btn btn-primary btn-learn" href="https://github.com/dbarochiya" target="_blank" rel="noopener noreferrer">View Projects <i className="icon-briefcase3" /></a></p>
+                          <h1 style={{ color:'black', background:'rgba(160,242,172,0.5)'}}>This is <br /> QAllaborative</h1>
                         </div>
                       </div>
                     </div>
                   </div>
                 </div>
               </li>
-              <li style={{backgroundImage: 'url(images/img_bg.jpg)'}}>
+              <li style={{backgroundImage: 'url(images/auckland_bg.jpg)'}}>
                 <div className="overlay" />
                 <div className="container-fluid">
                   <div className="row">
                     <div className="col-md-6 col-md-offset-3 col-md-pull-3 col-sm-12 col-xs-12 js-fullheight slider-text">
                       <div className="slider-text-inner">
                         <div className="desc">
-                          <h1>I often <br/>Write ... </h1>
-                          <p><a className="btn btn-primary btn-learn" href="https://dhruvbarochiya.me/" target="_blank" rel="noopener noreferrer">View Blog <i className="icon-book" /></a></p>
+                          <h1 style={{ color: 'black', background:'rgba(232,107,134,0.5)'}}>A kiwi based <br/>Test Engineering bootcamp!</h1>
                         </div>
                       </div>
                     </div>
