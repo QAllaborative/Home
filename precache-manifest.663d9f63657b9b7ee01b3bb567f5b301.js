be6aaa979328c55e26160f01eb2565118ee98d68
Pato Miner <patominer@gmail.com>
2020-11-03 00:04:38 +1300

    Updates

---
+++ precache-manifest.663d9f63657b9b7ee01b3bb567f5b301.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fcc6348fb9cfb8242b24",
+    "url": "/me/static/css/main.c8d17cd6.chunk.css"
+  },
+  {
+    "revision": "fcc6348fb9cfb8242b24",
+    "url": "/me/static/js/main.fcc6348f.chunk.js"
+  },
+  {
+    "revision": "1caf0789ee8fdb1f0b3c",
+    "url": "/me/static/js/runtime~main.1caf0789.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/me/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "4500f54b04212f0c04223cf907faf451",
+    "url": "/me/index.html"
+  }
+];
