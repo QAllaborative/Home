6306b67e80b6845938ad90320e9a5bbc091ef5b7
Pato Miner <patominer@gmail.com>
2020-11-02 23:10:47 +1300

    About updated

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -15,7 +15,8 @@ export default class About extends Component {
                     <h2 className="colorlib-heading">Creating the pool of professionals New Zealand needs!</h2>
                     <p>I'm a Senior SDET from Argentina living in New Zealand and loving every aspect of it! After 3 years of working in the local scene, I've decided to put my expertise and knowledge as a service to deliver the best testing solutions and enable a fast paced workflow!</p>
                     <p>I'm a professional coach and consultant based in the beautiful city of Wellington, where I teach the ways of Automation and Test Engineering, as well as audit and enhance the Automation mahi for my clients! </p>
-                    <p>Monthly, I'm hosting Automation Bootcamps and Workshops both through online webinas and physically across New Zealand. Check below for more details about dates, prices and what you will be learning! </p>
+                    <p>Find which branch of Testing you want to follow and grow your skills and expertice with real world applications with my help. Test Analyst? Test Engineer? Performance Tester? All of them are excellent options to get in the market!</p>
+                    <p>Be ready to apply to highly demanded job positions with top notch benefits and salaries in New Zealand and the world! Want to know more about why working as a Tester is perfect for you? Check this <a href="https://www.careers.govt.nz/jobs-database/it-and-telecommunications/information-technology/test-analyst/about-the-job" target="_blank" rel="noopener noreferrer">link!</a></p>
                     </div>
                 </div>
                 </div>
@@ -27,8 +28,8 @@ export default class About extends Component {
         <div className="colorlib-narrow-content">
             <div className="row">
             <div className="col-md-6 col-md-offset-3 col-md-pull-3 animate-box" data-animate-effect="fadeInLeft">
-                <span className="heading-meta">What I do?</span>
-                <h2 className="colorlib-heading">Here are some of my services</h2>
+                <span className="heading-meta">What we offer?</span>
+                <h2 className="colorlib-heading">Here are some of our services</h2>
             </div>
             </div>
             <div className="row row-pt-md">
@@ -40,7 +41,6 @@ export default class About extends Component {
                 <div className="desc">
                     <h3>E2E Automation </h3>
                     <p>More than 10 years developing Test solutions and Automating test cases using JavaScript, C#, Java/Groovy and Python with tools like Selenium and Cucumber, PyTest, Katalon Studio, TestProject, Cypress and TestCafe.</p>
-                    <p ><a className="btn btn-primary btn-learn" href="https://docs.google.com/document/d/15coSa2rnZcdywihM-pWTvoGqE65JAkxQAh6JDBNrF2Q/edit?usp=sharing" target="_blank" rel="noopener noreferrer">View CV<i className="icon-download4" /></a></p>
                 </div>
                 </div>
             </div>
@@ -52,7 +52,6 @@ export default class About extends Component {
                 <div className="desc">
                     <h3>Test Engineering</h3>
                     <p>I can build the whole Automation workflow from scratch and with different tools depending your needs. From frameworks for API Testing, to UI Automation, to the endgame of CI with Jenkins, Docker and dashboard tools for reporting.</p>
-                    <p><a className="btn btn-primary btn-learn" href="https://github.com/thefreerangetester" target="_blank" rel="noopener noreferrer">View Projects <i className="icon-briefcase3" /></a></p>
                 </div>
                 </div>
             </div>
@@ -64,7 +63,6 @@ export default class About extends Component {
                 <div className="desc">
                     <h3>Dev Ops</h3>
                     <p>Shifting the test approach left and right to have the right checks at the right time. From monitoring the use of Jenkins with Kibana, to display the user experience in Production with dashboards in Grafana, I can build the right tool for your DevOps testing needs.</p>
-                    <p><a className="btn btn-primary btn-learn" href="https://www.eventbrite.co.nz/e/road-to-test-engineer-automation-and-performance-bootcamp-tickets-126480050235" target="_blank" rel="noopener noreferrer">Next Automation Bootcamp <i className="icon-book" /></a></p>
                 </div>
                 </div>
             </div>
@@ -76,7 +74,6 @@ export default class About extends Component {
                 <div className="desc">
                     <h3>Teaching and career counsealing</h3>
                     <p>Helping New Zealand create the pool of engineers to cover the high demand. If you are looking for a new job to be passionate about, there is no better place to look at! </p>
-                    <p><a className="btn btn-primary btn-learn" href="emailto:thefreerangetester@gmail.com" target="_blank" rel="noopener noreferrer">Send an email <i className="icon-briefcase3" /></a></p>
                 </div>
                 </div>
             </div>
