d0ac71e78b2e6b320e72690ce3214e413e5c8445
Pato Miner <patominer@gmail.com>
2020-11-03 08:49:58 +1300

    Big commit!

---
+++ build/precache-manifest.de85699ce25064425c65a0f250703bfe.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a3f6981a11418f2bb851",
+    "url": "/qallaborative/static/css/main.c8d17cd6.chunk.css"
+  },
+  {
+    "revision": "a3f6981a11418f2bb851",
+    "url": "/qallaborative/static/js/main.a3f6981a.chunk.js"
+  },
+  {
+    "revision": "80b85436eccd0247feba",
+    "url": "/qallaborative/static/js/runtime~main.80b85436.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/qallaborative/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "66675c3f571e95574d31662da9d4d8a1",
+    "url": "/qallaborative/index.html"
+  }
+];
