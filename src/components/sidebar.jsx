fb9a04a9b6b54ad7aa8feb3a2e45def15eebffb0
Pato Miner <patominer@gmail.com>
2020-11-05 14:39:56 +1300

    added map marker icon

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -10,7 +10,8 @@ export default class Sidebar extends Component {
             <div className="text-center">
               <div className="author-img" style={{backgroundImage: 'url(images/io_suit.jpg)'}} />
               <h1 id="colorlib-logo"><a href="index.html">QAllaborative</a></h1>
-              <span className="email"><i className="icon-mail"><a href="mailto:thefreerangetester@gmail.com"> Contact me!</a></i></span><br></br>
+              <span className="email"><i className="icon-mail"><a href="mailto:thefreerangetester@gmail.com"> Contact me!</a></i></span>
+              <span className="email"><i className="fa fa-map-marker"> Wellington, New Zealand</i></span><br></br>
             </div>
             <nav id="colorlib-main-menu" role="navigation" className="navbar">
               <div id="navbar" className="collapse">
