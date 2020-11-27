78ad3306161971e68f381fb0be511d99a05adae6
Pato Miner <patominer@gmail.com>
2020-11-27 15:57:44 +1300

    cambio de logo

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -8,7 +8,7 @@ export default class Sidebar extends Component {
           <nav href="#navbar" className="js-colorlib-nav-toggle colorlib-nav-toggle" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar"><i /></nav>
           <aside id="colorlib-aside" className="border js-fullheight">
             <div className="text-center">
-              <div className="author-img" style={{backgroundImage: 'url(images/io_suit.jpg)'}} />
+              <div className="author-img" style={{backgroundImage: 'url(images/logo.jpg)'}} />
               <h1 id="colorlib-logo"><a href="index.html">QAllaborative</a></h1>
               <span className="email"><i className="icon-mail"><a href="mailto:thefreerangetester@gmail.com"> Contact me!</a></i></span>
               <span className="email"><i className="fa fa-map-marker"> Wellington, New Zealand</i></span><br></br>
