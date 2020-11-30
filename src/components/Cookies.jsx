49392c943a26dfa9ce0001603f883a40934e0c88
Pato Miner <patominer@gmail.com>
2020-11-30 14:17:58 +1300

    t

--- src/components/Cookies.jsx
+++ src/components/Cookies.jsx
@@ -9,14 +9,16 @@ export default class CookiesConsent extends Component {
                 buttonText="Accept"
                 cookieName="galletitasAutomation"
                 style={{ 
-                    background: "##DAF7A6",
+                    background: "#DAF7A6",
                     position: "fixed",
                     width: "100%",
-                    zIndex: "100"
-                
+                    zIndex: "999",
+
+
                 }}
                 buttonStyle={{ color: "#4e503b", fontSize: "13px" }}
                 expires={150}
+                overlay
             >
                 This website uses cookies to enhance the user experience.{" "}
                 
