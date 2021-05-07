b2e55780bb247f18c8006036af79801329ae3032
Pato Miner <patominer@gmail.com>
2021-05-07 18:35:29 +1200

    Vuelta a cero

--- src/App.js
+++ src/App.js
@@ -1,10 +1,4 @@
 import React, { Component } from 'react';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
 import './App.css';
 import Sidebar from './components/sidebar'
 import Introduction from './components/introduction'
@@ -12,13 +6,10 @@ import About from './components/about'
 import Timeline from './components/timeline'
 import Cards from './components/Cards'
 import Cookies from './components/Cookies'
-import Blog from './components/blog'
-import Projects from './components/projects'
 
 class App extends Component {
   render() {
     return (
-      <Router> 
       <div id="colorlib-page">
         <div id="container-wrap">
          	<Sidebar></Sidebar>
@@ -30,19 +21,7 @@ class App extends Component {
           <Cookies></Cookies>
           	</div>
       	</div>
-        <Switch>
-          <Route path="/about">
-            <About />
-          </Route>
-          <Route path="/users">
-            <Blog />
-          </Route>
-          <Route path="/">
-            <Projects />
-          </Route>
-        </Switch>
       </div>
-      </Router>
     );
   }
 }
