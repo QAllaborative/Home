2a8cec8a61473d8167583745727d85c4844e5adc
Pato Miner <patominer@gmail.com>
2021-06-02 14:35:03 +1200

    Rewording done

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -13,20 +13,15 @@ export default class About extends Component {
                     <div className="about-desc">
                     <span className="heading-meta">Who am I?</span>
                     <h2 className="colorlib-heading">Creating the pool of professionals New Zealand needs!</h2>
-                    <p>I'm a <strong>Senior Software Developer Engineer in Testing</strong> from Argentina living in New Zealand and loving every aspect of it! After 3 years of working in the local scene, I've decided to put my expertise and knowledge as a service to deliver the best testing solutions and enabling local testers and companies by teaching the ways of the Test Engineer!</p>
-                    <p>As a professional coach and consultant <strong>based in the beautiful city of Wellington</strong>, I offer holistic Automation and Test Engineering courses for people with no background and seasoned testers as well. As part of my consultancy work, I perform audits and enhance the Automation mahi(work) for my clients, delivering test assets based on the project's needs. </p>
+                    <p>I'm a <strong>Senior Software Developer Engineer in Testing</strong> from Argentina living in New Zealand and loving every aspect of it! After 4 years of working in the local scene, I've decided to put my expertise and knowledge as a service to deliver the best testing solutions and enabling local testers and companies by teaching the ways of the Test Engineer!</p>
+                    <p>As a professional coach and consultant <strong>based in the beautiful city of Wellington</strong>, I offer holistic Automation and Test Engineering courses and services for everyone. As part of my consultancy work, I perform audits to help enhance the Automation mahi (work) for my clients, delivering test assets based on each particular need. </p>
                     <p>Find which path of Software Testing you want to follow and grow your skills and experience with real world applications with my help. Test Analyst? Test Engineer? Performance Tester? All of them are excellent options to get into the market and start job hunting!</p>
                     <p>Be ready to apply to highly demanded positions with top notch benefits and salaries in New Zealand and the world! Want to know more about why working as a Tester is perfect for you? <strong>Check this</strong> <a href="https://www.careers.govt.nz/jobs-database/it-and-telecommunications/information-technology/test-analyst/about-the-job" target="_blank" rel="noopener noreferrer">link!</a></p>
-                    <h2 className="colorlib-heading">Invest in Automation training, for yourself or your team.</h2>
-                    <p>I understand the testing market in New Zealand and have vast experience teaching previous moving from overseas. This means that, <strong>if you are looking to get the skills to get into the market</strong>, there is no better place than here to start!</p>
-                    <p>My goal is to make learning Automation a <strong>fun, interactive experience</strong> for everyone and help every single student to achieve a robust, solid experience to start looking for jobs in this discipline.</p>
-                    <p><strong>Our country, our timezone:</strong> From local support through one on one calls, chats, meetings with a coffee (the best option!), to live workshops across New Zealand, you'll be supporting a kiwi based initiative to help other kiwis kickoff or take their Testing career to the next level! </p>
-                    <p><strong>Plenty of options to start learning:</strong> From webinars, to live workshops on the city, to on-site intensive trainings for entire Teams. <strong>There is an option for every need!</strong></p>
                     <h2 className="colorlib-heading">Test Engineering services, no matter how small or big your project is.</h2>
                     <p>Do you have a web/mobile application in development? <strong>Did you know you can get feedback about the state of your app faster with Automation?</strong> Ship all the goodness your customers want without delay!</p>
                     <p>From the creation of robust and scalable frameworks for your team to use,<strong> to live dashboards with scheduled tests running right when you need them,</strong> make your Software Development Life Cycle as efficient as it can be.</p>
                     <p>Seamlessly integrate with your development team by using Cypress with JavaScript, or get a Selenium based framework for your test team to use, my test tools stack covers all the main technologies being used professionally.</p>
-                    <p><strong>A free assessment is all I need</strong> to determine the best approach and implement the solution you need.</p>
+                    <p>I can assess and provide a <strong>quote</strong> to determine the best approach and implement the solution you need.</p>
                     </div>
                 </div>
                 </div>
@@ -38,8 +33,8 @@ export default class About extends Component {
         <div className="colorlib-narrow-content">
             <div className="row">
             <div className="col-md-6 col-md-offset-3 col-md-pull-3 animate-box" data-animate-effect="fadeInLeft">
-                <span className="heading-meta">Want to know more?</span>
-                <h2 className="colorlib-heading">HERE ARE SOME OF MY SERVICES</h2>
+                <span className="heading-meta">How can I help you?</span>
+                <h2 className="colorlib-heading">Knowledge as a Service</h2>
             </div>
             </div>
             <div className="row row-pt-md">
@@ -50,7 +45,7 @@ export default class About extends Component {
                 </span>
                 <div className="desc" data-animate-effect="fadeInLeft">
                     <h3>E2E Automation</h3>
-                    <p>No matter what your requirements are, I can do it. More than 10 years developing Test solutions using JavaScript, C#, Java/Groovy and Python with the main tools in the market, such as Selenium, PyTest, RestAssured, Cypress and more gives me a clear view of what works.</p>
+                    <p>No matter what your requirements are, I can do it. More than 10 years developing Test solutions using JavaScript, C#, Java/Groovy and Python with the main tools in the market, such as Selenium, PyTest, RestAssured, Cypress and more gives me flexibility to implement solutions.</p>
                 </div>
                 </div>
             </div>
@@ -83,7 +78,7 @@ export default class About extends Component {
                 </span>
                 <div className="desc" data-animate-effect="fadeInRight">
                     <h3>Test Engineering workshops</h3>
-                    <p>With the right guidance you can be doing real world test automation following the best practices, here, in New Zealand and with someone assisting you! Check the calendar or contact me for more information about upcoming workshops and other options to get you or your team aboard! </p>
+                    <p>Need to skill up your teams? Or maybe train people in your company? We can arrange a set of workshops onsite to provide real world education and training for your resources. Contact me and lets talk about what's your current status, your expected one and get your testing work to the next level! </p>
                 </div>
                 </div>
             </div>
@@ -94,7 +89,7 @@ export default class About extends Component {
                 </span>
                 <div className="desc">
                     <h3>Career counseling</h3>
-                    <p>If you are looking for a new job and you are currently a Tester (or even if you are not!), I can provide assistance with your CV, next steps and learnings and how to look for the perfect new gig. More than 8 years of experience doing technical interviews in the country gave me the knowledge to help you find your passion!</p>
+                    <p>If you are looking for a new job and you are currently a Tester (or even if you are not!), let's talk about your next steps! More than 8 years of experience doing technical interviews for technical roles in testing gave me the knowledge to help you find your passion through real actions and expertise!</p>
                 </div>
                 </div>
             </div>
@@ -105,7 +100,7 @@ export default class About extends Component {
                 </span>
                 <div className="desc">
                     <h3>On site training for teams</h3>
-                    <p>Avoid the common mistakes and pitfalls that lurk around every automation project. Get up and working with the best practices, solid frameworks and robust workflows to start delivering automation greatness today! Contact me to know about the options to get your team to the next automation level!</p>
+                    <p>Avoid the common mistakes and pitfalls that lurk around every automation project. Get your team up and running, ready to work with the best practices, solid frameworks and robust workflows to start delivering automation greatness today! There is always a way to make things right!</p>
                 </div>
                 </div>
             </div>
