227d99da00a2658070660b708b6c3605d7e84a71
Pato Miner <patominer@gmail.com>
2021-06-03 09:37:01 +1200

    Agregado el form

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,5 +1,11 @@
 import React, { Component } from 'react'
 
 export default class ContactForm extends Component {
-
+    render(){
+        return(
+            <div>
+            <div class="powr-contact-form" id="c3d2f88f_1622664138"></div><script src="https://www.powr.io/powr.js?platform=react"></script>
+            </div>    
+        )
+    }
 }
