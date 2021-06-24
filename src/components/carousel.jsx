84688e617a59747a67e5f6f4b10decb1d1233708
Pato Miner <patominer@gmail.com>
2021-06-25 11:44:14 +1200

    Styling, logos and testimonials.

--- src/components/carousel.jsx
+++ src/components/carousel.jsx
@@ -14,32 +14,32 @@ export default class Testimonials extends Component {
                 interval={6100}
             >
                 <div>
-                    <img src='images/logo.jpg' alt="cosos" />
+                    <img src='images/BNZ.png' alt="Banking testimonial" />
                     <div className="myCarousel">
-                        <h3>Jeco</h3>
-                        <h4>Senior dB Developer at BNZ</h4>
+                        <h3>J.J</h3>
+                        <h4>Senior Oracle Developer</h4>
                         <p>
                             One of the best testers I've worked with.
                         </p>
                     </div>
                 </div>
                 <div>
-                    <img src="images/img-4.jpg" alt="algo" />
+                    <img src="images/Udemy.jpeg" alt="Udemy student" />
                     <div className="myCarousel">
-                        <h3>Raul Sebastian</h3>
+                        <h3>Raul</h3>
                         <h4>Student from Udemy</h4>
                         <p>
-                            Excellent! He teaches really well and you can notice he knows a lot about what he is talking about.
+                            Excellent! He has the ability to explain complicated concepts in simple ways.
                         </p>
                     </div>
                 </div>
                 <div>
-                    <img src="images/img-5.jpg" alt="una imagen" />
+                    <img src="images/Accenture.jpg" alt="Accenture" />
                     <div className="myCarousel">
                         <h3>Edwyn</h3>
                         <h4>Test Manager at Accenture</h4>
                         <p>
-                            It was a pleasure working with him. He got the "At the very top" annual review on his first project with me!
+                            One of my top testers, which started the biggest automation project at that time in our branch.
                         </p>
                     </div>
                 </div>
