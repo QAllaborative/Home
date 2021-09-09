8bb0c03ea5c302a6a3be5bb7076bc8157d678fd1
Pato Miner <patominer@gmail.com>
2021-09-09 14:52:06 +1200

    prefill fields out

--- src/components/calendly.jsx
+++ src/components/calendly.jsx
@@ -15,29 +15,6 @@ export default class Calendly extends Component {
                     primaryColor: '00a2ff',
                     textColor: '4d5055'
                 }}
-                prefill={{
-                    customAnswers: {
-                        a1: 'a1',
-                        a10: 'a10',
-                        a2: 'a2',
-                        a3: 'a3',
-                        a4: 'a4',
-                        a5: 'a5',
-                        a6: 'a6',
-                        a7: 'a7',
-                        a8: 'a8',
-                        a9: 'a9'
-                    },
-                    date: new Date('2021-09-09T03:04:54.966Z'),
-                    email: 'test@test.com',
-                    firstName: 'Jon',
-                    guests: [
-                        'janedoe@example.com',
-                        'johndoe@example.com'
-                    ],
-                    lastName: 'Snow',
-                    name: 'Jon Snow'
-                }}
                 styles={{
                     display: "flex",
                     justifyContent: "center",
