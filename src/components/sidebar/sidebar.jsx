451a7836d5dfae19f799c1b79a5efca2414280f7
Pato Miner <patominer@gmail.com>
2024-06-11 14:21:49 +1200

    Mejoras en la UI

--- src/components/sidebar/sidebar.jsx
+++ src/components/sidebar/sidebar.jsx
@@ -1,6 +1,69 @@
-/* eslint-disable jsx-a11y/accessible-emoji */
-import React, { Component } from 'react';
-import './sidebar.css';
+import React, { Component } from "react";
+import "./sidebar.css";
+
+const AuthorInfo = () => (
+  <div className="text-center">
+    <div
+      className="author-img"
+      style={{ backgroundImage: "url(images/QAllaborative_logo_2_png.png)" }}
+    />
+    <h1 id="colorlib-logo">
+      <a href="#home">Pato Miner</a>
+    </h1>
+    <span className="location">
+      <i className="fa fa-map-marker location-icon"> Wellington, New Zealand</i>
+    </span>
+  </div>
+);
+
+const Navigation = ({ handleClick }) => (
+  <nav id="colorlib-main-menu" role="navigation" className="navbar">
+    <div id="navbar" className="collapse">
+      <ul>
+        <li className="active" onClick={handleClick}>
+          <a href="#home" data-nav-section="home">
+            Introduction
+          </a>
+        </li>
+        <li onClick={handleClick}>
+          <a href="#about" data-nav-section="about">
+            About
+          </a>
+        </li>
+        <li onClick={handleClick}>
+          <a href="#timeline" data-nav-section="timeline">
+            Background
+          </a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+);
+
+const SocialLinks = () => (
+  <nav id="colorlib-main-menu">
+    <ul>
+      <li>
+        <a
+          href="https://github.com/TheFreeRangeTester/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <i className="fa fa-github fa-4x" />
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://www.linkedin.com/in/patricio-m-690b3729/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <i className="fa-brands fa-linkedin fa-3x" />
+        </a>
+      </li>
+    </ul>
+  </nav>
+);
 
 export default class Sidebar extends Component {
   handleClick = (e) => {
@@ -11,62 +74,36 @@ export default class Sidebar extends Component {
 
   render() {
     return (
-      <div>
-        <div>
-          <nav
-            href="#navbar"
-            className="js-colorlib-nav-toggle colorlib-nav-toggle"
-            data-toggle="collapse"
-            data-target="#navbar"
-            aria-controls="navbar"
-          >
-            <i />
-          </nav>
-          <aside id="colorlib-aside" className="border js-fullheight">
-            <div className="text-center">
-              <div className="author-img" style={{
-                backgroundImage: 'url(images/QAllaborative_logo_2_png.png)',
-                width: '100%',
-                height: '200px'
-              }} />
-              <h1 id="colorlib-logo"><a href="#home">Pato Miner</a></h1>
-              <span className="location"><i className="fa fa-map-marker"> Wellington, New Zealand</i></span><br></br>
-            </div>
-            <nav id="colorlib-main-menu" role="navigation" className="navbar">
-              <div id="navbar" className="collapse">
-                <ul>
-                  <li className="active" onClick={this.handleClick}>
-                    <a href="#home" data-nav-section="home">
-                      Introduction
-                    </a>
-                  </li>
-                  <li onClick={this.handleClick}>
-                    <a href="#about" data-nav-section="about">
-                      About
-                    </a>
-                  </li>
-                  <li onClick={this.handleClick}>
-                    <a href="#timeline" data-nav-section="timeline">
-                      Background
-                    </a>
-                  </li>
-                </ul>
-              </div>
-            </nav>
-            <nav id="colorlib-main-menu">
-              <ul>
-                <li><a href="https://github.com/TheFreeRangeTester/" target="_blank" rel="noopener noreferrer"><i className="fa fa-github fa-4x" /></a></li>
-                <li><a href="https://www.linkedin.com/in/patricio-m-690b3729/" target="_blank" rel="noopener noreferrer"><i className="fa-brands fa-linkedin fa-3x" /></a></li>
-              </ul>
-            </nav>
-            <div className="colorlib-footer">
-              <p><small>
-                Made in <span role="img" aria-label="New Zealand">🇳🇿</span> by an imported <span role="img" aria-label="Argentina">🇦🇷</span><br></br>
-              </small></p>
-            </div>
-          </aside>
+      <>
+        <div
+          className="js-colorlib-nav-toggle colorlib-nav-toggle"
+          data-toggle="collapse"
+          data-target="#navbar"
+          aria-controls="navbar"
+          role="button"
+        >
+          <i />
         </div>
-      </div>
+        <aside id="colorlib-aside" className="border js-fullheight">
+          <AuthorInfo />
+          <Navigation handleClick={this.handleClick} />
+          <SocialLinks />
+          <div className="colorlib-footer">
+            <p>
+              <small>
+                Made in{" "}
+                <span role="img" aria-label="New Zealand">
+                  🇳🇿
+                </span>{" "}
+                by an imported{" "}
+                <span role="img" aria-label="Argentina">
+                  🇦🇷
+                </span>
+              </small>
+            </p>
+          </div>
+        </aside>
+      </>
     );
   }
 }
